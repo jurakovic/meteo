82671f31e957e9f6033aef2093718568afd87b80
jurakovic <17744091+jurakovic@users.noreply.github.com>
2024-09-07 00:50:05 +0200

    fix extras url

--- src/build.ps1
+++ src/build.ps1
@@ -47,7 +47,7 @@ function ProcessHtml() {
 	$html = Get-Content "$file" -Raw -Encoding "utf8"
 	$html = $html.Replace('<div class="links" data-include-html="/_components/links.html"></div>', "<div class=""links"">$links`t`t`t`t`t</div>")
 	$html = $html.Replace('href="/_assets/img', 'href="/meteo/img')
-	$html = $html.Replace('href="/extras/index.html', 'href="/extras/')
+	$html = $html.Replace('href="/extras/index.html', 'href="/meteo/extras/')
 	$html = $html.Replace('<link rel="stylesheet" href="/_assets/css/styles.css">', "<style>$css`t</style>")
 	$html = $html.Replace("<script src=""/_assets/js/main.js"" defer></script>", "<script>$js`t</script>")
 	$html = $html.Replace("<script src=""/_assets/js/include.js"" defer></script>`r`n`t", "") # removing this block
