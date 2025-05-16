320ff1eeb92bf2df56fb6c5aa017cd80de5adc7a
jurakovic <17744091+jurakovic@users.noreply.github.com>
2025-05-16 22:59:05 +0200

    njk backup

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,15 @@
 
 export default function(eleventyConfig) {
-  eleventyConfig.setInputDirectory("src");
-  eleventyConfig.setIncludesDirectory("_components");
-  eleventyConfig.setOutputDirectory("docs");
-	eleventyConfig.setTemplateFormats("html,liquid");
+	eleventyConfig.setInputDirectory("src");
+	eleventyConfig.setIncludesDirectory("_components");
+	eleventyConfig.setOutputDirectory("docs");
+	//eleventyConfig.setTemplateFormats("njk");
+	eleventyConfig.addFilter("indent", function(value, tabs = 1) {
+		console.log(tabs)
+		const pad = "\t".repeat(tabs);
+		return value
+			.split("\r\n")
+			.map(line => (line ? pad + line : line))
+			.join("\r\n");
+	});
 };
